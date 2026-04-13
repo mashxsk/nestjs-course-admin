44e113cc6f0e6ff00383db89667b8b50a94bf1d1
Maria <dutbdgdew@gmail.com>
2026-04-13 17:56:27 +0300

    Delete homeController.js

--- homeController.js
+++
@@ -1,9 +0,0 @@
-const Course = require("../models/course");
-
-exports.index = function(req, res) {
-    Course.findAll({ raw: true })
-        .then(data => {
-            res.render("index.hbs", { courses: data });
-        })
-        .catch(err => console.log(err));
-};
